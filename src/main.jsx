f2eae7113132b94f50a2e7da4a49778f88cd0897
JX-OctoNeko <YUjiaxiYU@outlook.com>
2025-10-09 18:55:24 +0800

    add speed insights func

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import './index.css'
 import CVHomeWrapper from './Combine'
@@ -9,5 +10,6 @@ import CVHomeWrapper from './Combine'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CVHomeWrapper />
+    <SpeedInsights />
   </StrictMode>,
 )
